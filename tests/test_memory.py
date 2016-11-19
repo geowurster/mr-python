4939bb8af8c3fbad34635adc872d17fc677a5feb
Kevin Wurster <geowurster@users.noreply.github.com>
2016-11-18 19:30:24 -0500

    Revert "Let MemMapReduce() operate as a generator"

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -3,11 +3,8 @@
 
 import itertools as it
 from multiprocessing.pool import ThreadPool
-import time
-import types
 import random
 
-from tinymr import bench
 from tinymr import errors
 from tinymr import memory
 from tinymr import tools
@@ -188,7 +185,7 @@ def test_MemMapReduce_exceptions():
 
     tmmk = TooManyMapperKeys()
     with pytest.raises(errors.KeyCountError):
-        tuple(tmmk([1]))
+        tmmk([1])
 
     class TooManyReducerKeys(memory.MemMapReduce):
         def mapper(self, item):
@@ -198,7 +195,7 @@ def test_MemMapReduce_exceptions():
 
     tmrk = TooManyReducerKeys()
     with pytest.raises(errors.KeyCountError):
-        tuple(tmrk([1]))
+        tmrk([1])
 
 
 def test_run_map_method(wordcount):
@@ -276,27 +273,4 @@ def test_MemMapReduce_check_keys(wordcount, tiny_text, method_name):
     setattr(wc, method_name, wc.checker)
 
     with pytest.raises(NameError):
-        tuple(wc(tiny_text))
-
-
-def test_MemMapReduce_generator(wordcount, tiny_text):
-
-    """By default the output should be a generator, and no work should happen
-    until the output is iterated over.
-    """
-
-    class WordCount(wordcount):
-
-        def mapper(self, line):
-            raise Exception
-
-        def reducer(self, key, values):
-            raise Exception
-
-    wc = WordCount()
-
-    with bench.timer() as elapsed:
-        output = wc(tiny_text.splitlines())
-
-    assert isinstance(output, types.GeneratorType)
-    assert elapsed.seconds < 1
+        wc(tiny_text)
--- tinymr/memory.py
+++ tinymr/memory.py
@@ -17,146 +17,87 @@ class MemMapReduce(base.BaseMapReduce):
     """In-memory MapReduce."""
 
     def _run_map(self, item):
-        """Run ``mapper()`` in a parallel context.  Ensures the iterator that
-        is likely produced by ``mapper()`` is expanded to a ``tuple()`` so it
-        can be pickled.
-        """
         return tuple(self.mapper(item))
 
-    def _partition_sort_mapped(self, mapped):
-
-        """Partition and sort data from the map phase.  Placing this in its
-        own method lets the entire MapReduce task be a generator.
-
-        Parameters
-        ----------
-        mapped : iter
-            Data from the map phase like ``(partition, sort, data)``.
-
-        Yields
-        ------
-        tuple
-            ``(key, (value, value, ...)``
-        """
-
-        partitioned = defaultdict(deque)
-        grouped = _compat.map(self._map_key_grouper, mapped)
-
-        # Only sort when required
-        if self.n_sort_keys == 0:
-            for ptn, val in grouped:
-                partitioned[ptn].append(val)
-            partitioned_items = tools.popitems(partitioned)
-        else:
-            for ptn, srt, val in grouped:
-                partitioned[ptn].append((srt, val))
-            if self.n_partition_keys > 1:
-                partitioned_items = it.starmap(
-                    lambda _ptn, srt_val: (_ptn[0], srt_val),
-                    partitioned.items())
-            else:
-                partitioned_items = tools.popitems(partitioned)
-
-        for i in partitioned_items:
-            yield i
-
     def _run_reduce(self, kv):
-        """Run ``reducer()`` in a parallel context."""
         key, values = kv
         if self.n_sort_keys != 0:
             values = sorted(values, key=op.itemgetter(0))
             values = _compat.map(op.itemgetter(1), values)
         return tuple(self.reducer(key, values))
 
-    def _partition_reduced(self, reduced):
-
-        """Partition data from ``reducere()``.  Placing this in its own method
-        lets the entire MapReduce task operate as a generator.
-
-        Parameters
-        ----------
-        reduced : iter
-            ``(key, value)``.
-        """
-
-        partitioned = defaultdict(deque)
-        for k, v in reduced:
-            partitioned[k].append(v)
-
-        for i in tools.popitems(partitioned):
-            yield i
-
-    def _check_keys(
-            self, checker, expected_key_count, phase, stream):
-
-        first = next(stream)
-        stream = it.chain([first], stream)
-        if len(first) != expected_key_count:
-            raise errors.KeyCountError(
-                "Expected {expected} keys from the {phase} phase, not "
-                "{actual} - first keys: {keys}".format(
-                    phase=phase,
-                    expected=expected_key_count,
-                    actual=len(first),
-                    keys=first))
-        checker(first)
-        for i in stream:
-            yield i
-
     def __call__(self, stream):
-
-        """Run the MapReduce task.
-
-        Parameters
-        ----------
-        stream : iter
-        """
 
         self.init_map()
 
         if self.map_jobs == 1:
             # Avoid the overhead (and debugging complexities) of
             # parallelized jobs
-            mapped = _compat.map(self.mapper, stream)
+            results = _compat.map(self.mapper, stream)
         else:
-            mapped = self._map_job_pool.imap_unordered(
+            results = self._map_job_pool.imap_unordered(
                 self._run_map,
                 stream,
                 self.map_chunksize)
-        mapped = it.chain.from_iterable(mapped)
+        results = it.chain.from_iterable(results)
 
         # Parallelized jobs can be difficult to debug so the first set of
         # keys get a sniff check for some obvious potential problems.
         # Exceptions here prevent issues with multiprocessing getting confused
         # when a job fails.
-        mapped = self._check_keys(
-            checker=self.check_map_keys,
-            expected_key_count=self.n_partition_keys + self.n_sort_keys + 1,
-            phase='map',
-            stream=mapped)
+        first = next(results)
+        results = it.chain([first], results)
+        expected_key_count = self.n_partition_keys + self.n_sort_keys + 1
+        if len(first) != expected_key_count:
+            raise errors.KeyCountError(
+                "Expected {expected} keys from the map phase, not {actual} - "
+                "first keys: {keys}".format(
+                    expected=expected_key_count,
+                    actual=len(first),
+                    keys=first))
+        self.check_map_keys(first)
 
-        # Partition and sort data from the map phase
-        partitioned_items = self._partition_sort_mapped(mapped)
+        partitioned = defaultdict(deque)
+        mapped = _compat.map(self._map_key_grouper, results)
+
+        # Only sort when required
+        if self.n_sort_keys == 0:
+            for ptn, val in mapped:
+                partitioned[ptn].append(val)
+            partitioned_items = partitioned.items()
+        else:
+            for ptn, srt, val in mapped:
+                partitioned[ptn].append((srt, val))
+            if self.n_partition_keys > 1:
+                partitioned_items = it.starmap(
+                    lambda _ptn, srt_val: (_ptn[0], srt_val),
+                    partitioned.items())
+            else:
+                partitioned_items = partitioned.items()
 
         # Reduce phase
         self.init_reduce()
         if self.reduce_jobs == 1:
-            reduced = _compat.map(self._run_reduce, partitioned_items)
+            results = _compat.map(self._run_reduce, partitioned_items)
         else:
-            reduced = self._reduce_job_pool.imap_unordered(
+            results = self._reduce_job_pool.imap_unordered(
                 self._run_reduce, partitioned_items, self.reduce_chunksize)
-        reduced = it.chain.from_iterable(reduced)
+        results = it.chain.from_iterable(results)
 
         # Same as with the map phase, issue a more useful error
-        reduced = self._check_keys(
-            checker=self.check_reduce_keys,
-            expected_key_count=2,
-            phase='reduce',
-            stream=reduced)
+        first = next(results)
+        results = it.chain([first], results)
+        if len(first) != 2:
+            raise errors.KeyCountError(
+                "Expected 2 keys from the reduce phase, not {} - first "
+                "keys: {}".format(len(first), first))
+        self.check_reduce_keys(first)
 
-        partitioned_items = self._partition_reduced(reduced)
+        partitioned = defaultdict(deque)
+        for k, v in results:
+            partitioned[k].append(v)
 
-        return self.output(partitioned_items)
+        return self.output(tools.popitems(partitioned))
 
 
 # Required to make ``MemMapReduce._run_map()`` pickleable.
