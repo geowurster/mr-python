c773e914861179cca5d7cc3bf324d17bdf8470a5
Kevin Wurster <wursterk@gmail.com>
2019-10-21 20:49:59 -0400

    Remove tinymr.tools.count_lines()

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,25 +1,9 @@
 """Unittests for ``tinymr.tools``."""
 
 
-import os
-
 import pytest
 
 from tinymr import tools
-
-
-def _icount_lines(path, minimum=1):
-
-    """
-    Count lines by opening the file and iterating over the file.
-    """
-
-    count = 0
-    with open(path) as f:
-        for l in f:
-            count += 1
-    assert count >= minimum
-    return count
 
 
 def test_slicer_even():
@@ -61,107 +45,6 @@ def test_mapkey():
 
     assert not isinstance(actual, (list, tuple))  # Make sure we get an iterator
     assert list(actual) == expected
-
-
-def test_count_lines_exception(linecount_file):
-
-    """
-    Make sure known exceptions in `count_lines()` are raised.
-    """
-
-    path = linecount_file()
-    with pytest.raises(ValueError):
-        tools.count_lines(path, linesep='too many chars')
-
-
-@pytest.mark.parametrize("linesep", ["\n", "\r\n"])
-def test_count_lines_small(linesep, linecount_file):
-
-    """
-    Count lines of a file that fits in the buffer.
-    """
-
-    path = linecount_file(linesep)
-    buff = os.stat(path).st_size + 2
-    assert _icount_lines(path) == tools.count_lines(
-        path, linesep=linesep, buffer=buff)
-
-
-@pytest.mark.parametrize("linesep", ["\n", "\r\n"])
-def test_count_lines_buffered(linesep, linecount_file):
-
-    """
-    Use the buffered method to count lines
-    """
-
-    path = linecount_file(linesep)
-    buff = os.stat(path).st_size // 4
-    assert _icount_lines(path) == tools.count_lines(
-        path, linesep=linesep, buffer=buff)
-
-
-def test_count_lines_split_buffer(tmpdir):
-
-    """
-    Explicitly test a scenario where the `linesep` character is 2 bytes long
-    and is split across blocks.
-    """
-
-    path = str(tmpdir.mkdir('test_count_lines').join('split_buffer'))
-    with open(path, 'wb') as f:
-        f.write(b'\r\nhey some words')
-    assert tools.count_lines(path, buffer=1, linesep='\r\n') == 1
-
-
-def test_count_lines_literal_linesep(tmpdir):
-
-    """
-    Explicitly test a scenario where the input file contains a literal '\n'.
-    """
-
-    path = str(tmpdir.mkdir('test_count_lines').join('literal_linesep'))
-    with open(path, 'w') as f:
-        f.write('first line with stuff' + os.linesep)
-        f.write('before \{} after'.format(os.linesep) + os.linesep)
-    assert tools.count_lines(path) == 3
-
-
-def test_count_lines_empty(tmpdir):
-
-    """
-    Completely empty file.
-    """
-
-    path = str(tmpdir.mkdir('test_count_lines').join('empty'))
-    with open(path, 'w') as f:
-        pass
-    assert tools.count_lines(path) == 0
-
-
-def test_count_lines_only_linesep(tmpdir):
-
-    """
-    File only contains a `linesep`.
-    """
-
-    path = str(tmpdir.mkdir('test_count_lines').join('only_linesep'))
-    with open(path, 'w') as f:
-        f.write(os.linesep)
-    assert tools.count_lines(path) == 1
-
-
-def test_count_lines_trailing_linesep(tmpdir):
-
-    """
-    Last line has a trailing `linesep`.
-    """
-
-    path = str(tmpdir.mkdir('test_count_lines').join('trailing_linesep'))
-    with open(path, 'w') as f:
-        f.write('line1' + os.linesep)
-        f.write('line2' + os.linesep)
-        f.write('line3' + os.linesep)
-    assert tools.count_lines(path) == 3
 
 
 def test_popitems():
--- tinymr/tools.py
+++ tinymr/tools.py
@@ -2,8 +2,6 @@
 
 
 import itertools as it
-import io
-import os
 
 from tinymr import _compat
 
@@ -71,93 +69,6 @@ def mapkey(key, values):
     """
 
     return _compat.zip(it.repeat(key), values)
-
-
-def count_lines(
-        path,
-        buffer=io.DEFAULT_BUFFER_SIZE,
-        linesep=os.linesep,
-        encoding='utf-8'):
-
-    """
-    Quickly count the number of lines in a text file.  Useful for computing an
-    optimal `chunksize`.
-
-    Comparable to `$ wc -l` for files larger than ``~100 MB``, and significantly
-    faster as the file gets smaller (ignoring Python interpreter startup and
-    imports).  For reference just looping over all the lines in a 1.2 GB file
-    takes ~6 or 7 seconds, but `count_lines()` takes ~1.5.
-
-    For reference just looping over all the lines in the ``1.2 GB`` file takes
-    ``~6 to 7 sec``.
-
-    Speed is achieved by reading the file in blocks and counting the occurrence
-    of `linesep`.  For `linesep` strings that are larger than 1 byte we
-    check to make sure a `linesep` was not split across blocks.
-
-    Scott Persinger on StackOverflow gets credit for the core logic.
-    http://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
-
-    Parameters
-    ----------
-    path : str
-        Path to input text file.
-    buffer : int, optional
-        Buffer size in bytes.
-    linesep : str, optional
-        Newline character.  Cannot be longer than 2 bytes.
-    encoding : str, optional
-        Encoding of newline character so it can be converted to `bytes()`.
-
-    Returns
-    -------
-    int
-    """
-
-    nl = bytearray(linesep.encode(encoding))
-    size = os.stat(path).st_size
-
-    if len(nl) > 2:
-        raise ValueError(
-            "Cannot handle linesep characters larger than 2 bytes.")
-
-    # File is small enough to just process in one go
-    elif size < buffer:
-        with open(path, 'rb', buffering=buffer) as f:
-            return getattr(f, 'raw', f).read(buffer).count(nl)  # No raw in PY2
-
-    # Process in chunks
-    # Number of chunks is pre-determined so that the last chunk can be
-    # read into a fresh array to avoid double-counting
-    else:
-
-        buff = bytearray(buffer)
-        blocks = size // buffer
-        lines = 0
-
-        with open(path, 'rb') as f:
-
-            # Optimize the loops a bit in case we are working with a REALLY
-            # big file
-            readinto = getattr(f, 'raw', f).readinto  # no raw in PY2
-            count = buff.count
-
-            for i in range(blocks):
-                readinto(buff)
-                lines += count(nl)
-
-                # linesep is something like \r\n, which means it could be split
-                # across blocks, so we need an additional check
-                if buff[-1] == nl[0]:
-                    lines += 1
-
-            # The last bit of data in the file is smaller than a block
-            # We can't just read this into the constant buffer because
-            # it the remaining bytes would still be populated by the
-            # previous block, which could produce duplicate counts.
-            lines += getattr(f, 'raw', f).read().count(nl)  # No raw in PY2
-
-        return lines
 
 
 def popitems(dictionary):
